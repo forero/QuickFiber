840b5ff71d0a0d4795aa70280b6be0feb83f8fdd
Jaime Forero <j.e.forero.romero@gmail.com>
2014-09-25 13:59:06 -0500

    command line option to give the plate index

--- python/QuickFiber.py
+++ python/QuickFiber.py
@@ -1,4 +1,10 @@
 import FiberAllocation as FA
+import sys, string, os
+
+min_index = sys.argv[1]
+max_index = sys.argv[2]
+
+print range(int(min_index), int(max_index))
 
 #load all the data
 objects = FA.ObjectCatalog(filein="../data/desi/objects0.rdzipn")
@@ -11,6 +17,6 @@ objects.healpixelize(n_side)
 tiles.healpixelize(n_side)
 
 #find the available galaxies for the first 20 plates
-for i in range(20):
+for i in range(24*60):
     x, y = FA.find_available_galaxies(fibers, tiles, objects,i)
     print i
